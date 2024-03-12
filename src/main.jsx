5a110d30384a86283265a4501505cd6ead91e249
etienneDev <etienne.baillif97436@gmail.com>
2024-03-12 21:03:34 +0400

    routage

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Avis from './Avis.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from  'react-router-dom'
 
+const routes = createBrowserRouter([
+  {path: '/',
+  element: <App></App>
+  },
+  {
+    path: 'avis',
+    element: <Avis></Avis>   
+  }
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Avis />
+    <RouterProvider router={routes}/>
   </React.StrictMode>,
 )
