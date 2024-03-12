8bc38f344b6b208b026cefa632439efb79865057
etienneDev <etienne.baillif97436@gmail.com>
2024-03-12 19:46:37 +0400

    like mise a jours

--- src/component/AvisItem.jsx
+++ src/component/AvisItem.jsx
@@ -1,27 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 function AvisItem({ avis }) {
-
   //Logique de gestion des likes
-  const [favoris, setFavoris] = useState(false);
-  const handleFavoris = () => {
-    setFavoris(!favoris);
+  const etat = avis.type === 'POSITIF'; //true ou false suivant type
+  const [favoris, setFavoris] = useState(etat); //initialise favoris avec etat
+  console.log("avis.type" + avis.type + "id" + avis.id + "favoris" + favoris);
+
+  // useEffect(() => {
+  //   console.log("Use effect ! ");
+    
+  // }, [])
+
+  const handleFavoris = async () => {
+    try {
+      // Inverser l'état de favoris et obtenir sa valeur actuelle
+      setFavoris(prevFavoris => {
+        return !prevFavoris;
+      });
+  
+      //Récupération de l'identifiant du commentaire
+      const commentaireId = avis.id;
+      const typeAvis = !favoris ? 'POSITIF' : 'NEGATIF'; // Utilisation de la nouvelle valeur de favoris
+  
+      //Requête PUT vers API
+      const response = await fetch(`http://localhost:8080/api/avis/${commentaireId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          type: typeAvis,
+        }),
+      });
+  
+      if (!response.ok) {
+        // La requête a échoué (code de statut autre que 2xx)
+        console.error('Erreur lors de la requête PUT :', response.status, response.statusText);
+      }
+    } catch (error) {
+      // Gérer les erreurs de la requête
+      console.error('Erreur lors de la requête PUT :', error);
+    }
   };
+  
 
   return (
     <div key={`avis-${avis.id}`} className='p-4 rounded-md bg-slate-50 text-black'>
       <p className='flex justify-end items-center'>
-        {/*Utilisation d'une condition ternaire pour afficher le bouton favori */}
-          <button type='button' onClick={handleFavoris}>
-            {favoris ?(
-                 <span className='text-2xl'>&#9829;</span>
-            ):(
-                 <span className='text-2xl'>&#9825;</span>
-            )}
-   
-           </button>
-
+        {/* Utilisation d'une condition ternaire pour afficher le bouton favori */}
+        <button type='button' onClick={handleFavoris}>
+          {favoris ? (
+            <span className='text-2xl'>&#9829;</span>
+          ) : (
+            <span className='text-2xl'>&#9825;</span>
+          )}
+        </button>
       </p>
       <div className='texte'>{avis.avis}</div>
       <h4 className='texte'>{avis.client.email}</h4>
